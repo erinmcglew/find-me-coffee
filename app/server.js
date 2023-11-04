a812354c5b2efa1e15107e7ea253e3228e9cebf4
Erin McGlew <em3223@drexel.edu>
2023-11-04 13:29:08 -0400

    serving site dynamically so api key can change

--- app/server.js
+++ app/server.js
@@ -8,16 +8,48 @@ const hostname = "localhost";
 const env = require("../env.json");
 const Pool = pg.Pool;
 const pool = new Pool(env);
+
+//https://codedamn.com/news/javascript/fix-require-is-not-defined
+let myKeys = require("../key.json");
+let userKey = myKeys.key;
+
 pool.connect().then(function () {
   console.log(`Connected to database ${env.database}`);
 });
 
 app.use(express.static("public"));
 
-/* YOUR CODE HERE */
-
 app.get('/', (req, res) => {
-  res.send('index.html');
+  //serving site dynamically so the api key can change per user
+  //include mapbox libraries in html: https://docs.mapbox.com/mapbox.js/api/v3.3.1/ 
+  //using the map class: https://docs.mapbox.com/mapbox-gl-js/guides/ 
+  res.send(`
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset='utf-8' />
+    <title>Map of Philadelphia</title>
+    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.7.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.7.0/mapbox-gl.css' rel='stylesheet' />
+    <style>
+      body { margin: 0; padding: 0; }
+      #map { position: absolute; top: 0; bottom: 0; width: 100%; }
+    </style>
+  </head>
+  <body>
+    <div id='map'></div>
+    <script>
+      mapboxgl.accessToken = "${userKey}";
+      var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-75.1639, 39.9526], // starting position [lng, lat] of Philadelphia
+        zoom: 12
+      });
+    </script>
+  </body>
+  </html>`)
 })
 
 app.listen(port, hostname, () => {
