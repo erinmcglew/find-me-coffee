0e177076c7acf17399cb41b6aaf9fb7713184596
zac-ng <zac.ng201@gmail.com>
2023-11-04 22:35:51 -0400

    Refactor to use .env file

--- app/server.js
+++ app/server.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const pg = require("pg");
 const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
 
-const port = 3000;
-const hostname = "localhost";
+// load enviromental variables
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-const env = require("../env.json");
+const port = process.env.PORT;
+const hostname = process.env.HOSTNAME;
+
+// Initialize database
 const Pool = pg.Pool;
-const pool = new Pool(env);
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.HOSTNAME,
+  database: process.env.DATABASE,
+  password: process.env.DATABSE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+
 pool.connect().then(function () {
-  console.log(`Connected to database ${env.database}`);
+  console.log(`Connected to database `);
 });
 
 app.use(express.static("public"));
-
-/* YOUR CODE HERE */
 
 app.get('/', (req, res) => {
   res.send('index.html');
