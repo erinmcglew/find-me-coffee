4d14d04aa1a9f88f5ec8de3866573ae1981ca4a7
Erin McGlew <em3223@drexel.edu>
2023-11-07 10:39:34 -0500

    using map access token in .env

--- app/server.js
+++ app/server.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
+const mapbox_access_token = process.env.MAPBOX_ACCESS_TOKEN;
 
 // Initialize database
 const Pool = pg.Pool;
@@ -102,12 +103,115 @@ app.get('/logout', (req, res) => {
   });
 });
 
+
 app.get('/'), (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 }
 
+//removed this so we can send the page dynamically
+// app.get('/map', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'map.html'));
+// });
+
+//serving the map page dynamically, so users can use their personal map_access_token defined in their .env
 app.get('/map', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'map.html'));
+  res.send(`<html>
+  <head>
+    <title>Find Me Coffee</title>
+
+    <link
+      href="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css"
+      rel="stylesheet"
+    />
+    <script src="https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js"></script>
+    <link
+      href="https://api.mapbox.com/mapbox-assembly/v1.3.0/assembly.min.css"
+      rel="stylesheet"
+    />
+    <script
+      id="search-js"
+      defer=""
+      src="https://api.mapbox.com/search-js/v1.0.0-beta.17/web.js"
+    ></script>
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+      }
+      #map {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    Find me coffee app. Render mapbox HTML map here.
+  </body>
+  <div id="map"></div>
+  <mapbox-search-box
+    access-token="${mapbox_access_token}"
+    proximity="0,0"
+  >
+  </mapbox-search-box>
+  <script>
+    const ACCESS_TOKEN = "${mapbox_access_token}";
+    console.log(ACCESS_TOKEN);
+    mapboxgl.accessToken = ACCESS_TOKEN;
+
+    //loads the map
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v12", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    // Create a new marker.
+    //const marker = new mapboxgl.Marker().setLngLat([30.5, 50.5]).addTo(map);
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+    //Set marker options.
+    const marker = new mapboxgl.Marker({
+      color: "#ffffff",
+      draggable: true,
+    })
+      .setLngLat([30.5, 50.5])
+      .addTo(map);
+
+    const searchJS = document.getElementById("search-js");
+    searchJS.onload = function () {
+      const searchBox = new MapboxSearchBox();
+      searchBox.accessToken = ACCESS_TOKEN;
+      searchBox.options = {
+        types: "address,poi",
+        proximity: [-73.99209, 40.68933], //see if this is long/latitude
+      };
+      searchBox.marker = true;
+      searchBox.mapboxgl = mapboxgl;
+      map.addControl(searchBox);
+    };
+
+    // Initialize the GeolocateControl.
+    const geolocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+    });
+
+    // After the map is loaded, trigger the geolocate control to automatically show the user's location
+    map.on("load", function () {
+      geolocate.trigger();
+    });
+
+    // Add geolocate control to the map
+    map.addControl(geolocate);
+
+  </script>
+</html>
+`);
 });
 
 app.get('/login', (req, res) => {
