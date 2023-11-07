a57e439780b5a604ba063aa3734b0dd1e961c83c
zac-ng <zac.ng201@gmail.com>
2023-11-06 21:58:56 -0500

    Added google authentication. Refactored routes so map is acessible via /map instead of base url. Added skeleton for coffee homepage

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # find-me-coffee
 
-1. Change the sample.env.json file to your postgres username and password and change the name of the sample.env.json to env.json
-2. Use npm run setup to create database. This runs the setup (on wsl the command is sudo -u postgres psql -f setup.sql). This command may vary based on distribution.
-3. Installation modules using npm i
-4. Use npm run start to start server. No need to change directory to app/public and then run node server.js
+1. Change the sample.env file to your postgres username and password and change the name of the sample.env to .env
+2. Generate a OAuth Client ID and secret using a google developer account, and store it inside the .env file.
+3. Take the Google Callback URL, and add it as an authorized redirect URL.
+4. Use npm run setup to create database. This runs the setup (on wsl the command is sudo -u postgres psql -f setup.sql). Do not forget to start your server using sudo service postgresql start. This command may vary based on distribution.
+5. Installation modules using npm i
+6. Use npm run start to start server. No need to change directory to app/public and then run node server.js
--- app/server.js
+++ app/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const session = require("express-session")
 const pg = require("pg");
 const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
+const passport = require('passport');
 
 // load enviromental variables
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+app.use(express.static("public"));
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
@@ -16,7 +20,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.HOSTNAME,
   database: process.env.DATABASE,
-  password: process.env.DATABSE_PASSWORD,
+  password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
 });
 
@@ -24,11 +28,91 @@ pool.connect().then(function () {
   console.log(`Connected to database `);
 });
 
-app.use(express.static("public"));
+// Setup authentication strategies
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-  res.send('index.html');
-})
+// Passport Session
+app.use(session({ 
+  secret: process.env.SESSION_SECRET, 
+  resave: true,
+  saveUninitialized: true 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Load client secrets
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const CALLBACK_URL = 'http://localhost:3000/auth/google/callback';
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Store user in database here later
+      return done(null, profile);
+    }
+  )
+);
+
+// Serialize and deserialize the user after hearing back from google
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use('/map', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    // Contains user info
+    console.log(req.user);
+    return next();
+  }
+  res.redirect('/login');
+});
+
+// Define the route for Google OAuth login
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+);
+
+// Define the callback route
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/map',
+    failureRedirect: '/',
+  })
+);
+
+// Route to Logout and end session
+app.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect('/');
+  });
+});
+
+app.get('/'), (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+}
+
+app.get('/map', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'map.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
 app.listen(port, hostname, () => {
   console.log(`Listening at: http://${hostname}:${port}`);
