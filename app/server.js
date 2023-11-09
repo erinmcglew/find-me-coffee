d94e996e77ee15d5bbbdf8c467106b2ea64f2607
YoBismarck20 <wc492@drexel.edu>
2023-11-09 13:07:59 -0500

    more correct path

--- app/server.js
+++ app/server.js
@@ -107,9 +107,9 @@ app.get('/logout', (req, res) => {
 });
 
 
-//app.get(('/'), (req, res) => {
-//  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//});
+app.get(('/'), (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get("/addReview",(req,res)=>{
   
