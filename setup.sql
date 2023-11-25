78d203c2ff7ddfd8dc27374e9c9e254ea761c9f4
Erin McGlew <em3223@drexel.edu>
2023-11-25 14:04:40 -0500

    fixes for default review feed

--- setup.sql
+++ setup.sql
@@ -4,16 +4,16 @@ CREATE DATABASE findmecoffee;
 CREATE TABLE shops (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
-	location VARCHAR(25)
+	location VARCHAR(100)
 );
 CREATE TABLE users (
-    id VARCHAR(25),
-    username VARCHAR(25)
+    id VARCHAR(100),
+    username VARCHAR(100)
 );
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     shop_id INT REFERENCES shops(id),
-    user_id VARCHAR(25),
+    user_id VARCHAR(100),
     rating FLOAT,
     comments TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
